74548f41273a0216b721853fa926d46e11fcf55b
Zach Chaco <zjchaco12@gmail.com>
2018-06-11 11:34:04 -0700

    Added Tanks tutorial and Project CTA

---
+++ Tanks/Unity/Assets/Scripts/Tank/TankClient.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class TankClient : MonoBehaviour {
+
+    Thread mThread;
+    public string connectionIP = "127.0.0.1";
+    public int connectionPort = 25001;
+    IPAddress localAdd;
+    TcpListener listener;
+    TcpClient client;
+    Vector3 pos = Vector3.zero;
+
+    bool running;
+	// Use this for initialization
+	void Start () {
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread = new Thread(ts);
+        mThread.Start();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = pos;
+	}
+
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        Debug.Log(host.HostName);
+        foreach(var ip in host.AddressList)
+        {
+            if(ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No Network adapters with IPV4 address in system!");
+    }
+
+    void GetInfo()
+    {
+        localAdd = IPAddress.Parse(connectionIP);
+        listener = new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+
+        client = listener.AcceptTcpClient();
+
+        running = true;
+        while (running)
+        {
+            Connection();
+        }
+        listener.Stop();
+    }
+
+    void Connection()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        if (dataReceived != null)
+        {
+            if (dataReceived == "stop")
+            {
+                running = false;
+            }
+            else
+            {
+                pos = 10f * StringToVector3(dataReceived);
+                print("moved");
+                nwStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+    public static Vector3 StringToVector3(string sVector)
+    {
+        //Remove parenthesis
+        if(sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+
+        string[] sArray = sVector.Split(',');
+
+        //store as a vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2])
+            );
+
+        return result;   
+    }
+
+}   
---
+++ Tanks/Unity/Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;       
+    public Rigidbody m_Shell;            
+    public Transform m_FireTransform;    
+    public Slider m_AimSlider;           
+    public AudioSource m_ShootingAudio;  
+    public AudioClip m_ChargingClip;     
+    public AudioClip m_FireClip;         
+    public float m_MinLaunchForce = 15f; 
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f;
+
+    
+    private string m_FireButton;         
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+    
+
+    private void Update()
+    {
+        // Track the current state of the fire button and make decisions based on the current launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && m_Fired)
+        {
+            //at max charge, not fired
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+
+        }
+
+        else if(Input.GetButtonDown(m_FireButton))
+        {
+            //when button is first pressed
+            m_Fired = false;
+
+            //start charging
+            m_CurrentLaunchForce = m_MinLaunchForce;
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            //as button is being held down
+            //adjust aim slider and charge speed
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+            m_AimSlider.value = m_CurrentLaunchForce;      
+        }
+        else if(Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            //we release the button
+            Fire();
+        }
+
+    }
+
+
+    private void Fire()
+    {
+        // Instantiate and launch the shell.
+        m_Fired = true;
+        //Instantiate as rigidbody 
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        m_CurrentLaunchForce = m_MinLaunchForce;
+
+    }
+}
